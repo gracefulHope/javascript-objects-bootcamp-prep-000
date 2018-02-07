b4564703140cba809fcbb27279f7b86aada48019
Sara S <ewf.gloria@gmail.com>
2018-02-07 20:07:39 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {"Blue Man Group": "Shadows"}
 
 function updatePlaylist (cList, artist, song){
-  Object.assign(cList, {artist: song})
+  Object.assign(cList, {[artist]: song})
   return cList
 }
